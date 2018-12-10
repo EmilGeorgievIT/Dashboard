8b366b3acf05e94f9fb2f3d03fc6b3725134fce9
Emil <emil_georgiev_it@abv.bg>
2018-12-10 12:52:11 +0000

    Add datepicker

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,50 +1,83 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Chart } from 'chart.js';
+import {
+  MatDatepickerModule,
+  MatNativeDateModule,
+  MatFormFieldModule,
+  MatInputModule
+} from '@angular/material';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Dashboard';
   name = "Emil";
-  chart : any;
-  
+  chart: any;
+  chartLine: any;
   constructor() {
-   }
-  
+  }
+
   ngOnInit() {
-  	this.chart = new Chart('chart', {
-          type: 'line',
-          data: {
-            labels: ['a','b','c'],
-            datasets: [
-              {
-                data: ['12','33'],
-                borderColor: '#3cba9f',
-                fill: false
-              },
-              {
-                data: ['23','33'],
-                borderColor: '#ffcc00',
-                fill: false
-              },
-            ]
-          },
-          options: {
-            legend: {
-              display: false
-            },
-            scales: {
-              xAxes: [{
-                display: true
-              }],
-              yAxes: [{
-                display: true
-              }]
-            }
+    this.chart = new Chart('chart', {
+      type: 'line',
+      data: {
+        labels: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
+        datasets: [
+          {
+            data: ['1', '3', '2', '5', '5', '9'],
+            borderColor: '#3cba9f',
+            fill: false
           }
-        })
-	}
+        ]
+      },
+      options: {
+        legend: {
+          display: false
+        },
+        scales: {
+          xAxes: [{
+            display: true
+          }],
+          yAxes: [{
+            display: true
+          }]
+        }
+      }
+    })
+
+    this.chartLine = new Chart('chartLine', {
+      type: 'bar',
+      data: {
+        labels: ['In progress', 'Pending', 'Finished'],
+        datasets: [
+          {
+            data: ['12', '12', '2'],
+            borderColor: '#3cba9f',
+            fill: false
+          },
+          {
+            data: ['23', '12', '22'],
+            borderColor: '#ffcc00',
+            fill: false
+          },
+        ]
+      },
+      options: {
+        legend: {
+          display: false
+        },
+        scales: {
+          xAxes: [{
+            display: true
+          }],
+          yAxes: [{
+            display: true
+          }]
+        }
+      }
+    })
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatIconModule } from '@angular/material/icon';
+import {
+  MatDatepickerModule,
+  MatNativeDateModule,
+  MatFormFieldModule,
+  MatInputModule
+} from '@angular/material';
+
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
+    MatDatepickerModule,
+    MatFormFieldModule,
+    MatNativeDateModule,
     MatIconModule
   ],
-  providers: [],
+  providers: [MatDatepickerModule],
   bootstrap: [AppComponent]
 })
 export class AppModule {
